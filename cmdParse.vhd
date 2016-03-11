420dffc70692b347aaf75dad89d866cda4f3ea68
iyop45 <roy.miles12@gmail.com>
2016-03-11 14:58:27 +0000

    Fixed port mapping

--- cmdParse.vhd
+++ cmdParse.vhd
@@ -1,3 +1,8 @@
+---
+--- author: roy miles
+--- processing the commands send from Rx
+---
+
 library ieee;
 use ieee.std_logic_1164.all;
 --use ieee.std_logic_arith.all;
@@ -11,23 +16,19 @@ entity cmdParse is
 	port (
 		clk:		in std_logic;
 		reset:		in std_logic;
+		
 		rxnow:		in std_logic; -- valid port
 		rxData:			in std_logic_vector (7 downto 0);
-		txData:			out std_logic_vector (7 downto 0);
 		rxdone:		out std_logic;
-		ovErr:		in std_logic;
-		framErr:	in std_logic;
+		
+		txData:			out std_logic_vector (7 downto 0);
 		txnow:		out std_logic;
 		txdone:		in std_logic;
-		start: out std_logic;
-		--numWords: out std_logic_vector(9 downto 0);
 		numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
-		dataReady: in std_logic;
-		byte: in std_logic_vector(7 downto 0);
-		maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
-		--dataResults: in std_logic_vector(55 downto 0);
-		dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
-		seqDone: in std_logic
+		
+		cmdNow: out std_logic;     
+		cmdDone: in std_logic; 
+		cmdRecieve: in std_logic
 	);
 end cmdParse;
 
@@ -49,10 +50,6 @@ architecture parseCommands of cmdParse is
 	signal counter_enable : std_logic := '0';
 	signal counter_reset : std_logic := '0';
 	signal count : integer := 0;
-	--our signals
-	signal cmdNow : std_logic := '0';
-	signal cmdDone : std_logic := '0';
-	signal cmdRecieve : std_logic := '0';
 begin
   
   combi_nextState: process(clk, curState)
