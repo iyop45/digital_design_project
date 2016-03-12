89507dec67e0bba11fab966e2f877bfd9653459f
phgorman <phgorman@gmail.com>
2016-03-12 14:48:13 +0000

    Added comments and made it cleaner.

--- dataConsume.vhd
+++ dataConsume.vhd
@@ -1,12 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic
+use ieee.numeric_std_unsigned.all;
 use work.common_pack.all;
 
 entity dataConsume is
   	port (
-	        clk:in std_logic;
-		reset:in std_logic; -- synchronous reset
+	  clk:		in std_logic;
+		reset:		in std_logic; -- synchronous reset
 		start: in std_logic; -- goes high to signal data transfer
 		numWords_bcd: in BCD_ARRAY_TYPE(2 downto 0);
 		ctrlIn: in std_logic;
@@ -25,25 +25,44 @@ architecture detectorArch of dataConsume is
   type state_type is (S0, S1, S2, S3, S4);
   signal curState, nextState: state_type;
   signal peakvalue, newValue: std_logic_vector(7 downto 0) := "00000000";
-  signal equal, peakValueSmaller, shift_enable, store_enable, count_enable, loop_enable, count_reset : bit := '0';
+  signal ctrlOut_reg, equal, peakValueSmaller, shift_enable, store_enable, count_enable, count_reset, start_enable : std_logic:='0';  --bit := '0';
   signal ctrlIn_delayed, ctrlIn_detected: std_logic;
-  signal ctrlOut_reg: std_logic :='0';
-  --signal finalResults: CHAR_ARRAY_TYPE(0 to 6);
   signal allData: CHAR_ARRAY_TYPE(0 to 999);  
-  signal indexpk, index, start_index: integer;
-  signal numWordsValue: integer := 0;
-  signal start_enable: bit ; --signal that observes start
+  signal indexpk, index, start_index, numWordsValue: integer:=0;
+  
+  --curState - Current State.
+  --nextState - next State.
+  --peakvalue - Current Peak Value.
+  --newValue - The most recent value that has been recieved from the data generator.
+  --ctrlOut_reg - Holds the value of the ctrlOut in a register one clock cycle behind ctrlOut.
+  --equal - Goes high when the current peak value and the most recent value from the data genator ire the same.
+  --peakValueSmaller - Goes high when the peak value is smaller then the small value.
+  --shift_enable - Goes high to enable the shifter.
+  --store_enable - Goes high to enable the final values to be stored in the dataResults Array.
+  --count_enable - Goes high to enable the counter.
+  --count_reset - A reset for the counter. 
+  --start_enable - Goes high when a start signal has been sent.
+  --ctrlIn_delayed - Holds the value of the ctrlIn by one delayed clock cycle.
+  --ctrlIn_detected - Goes high when the value of CtrlIn and ctrlIn_delayed are different.
+  --allData - Stores all of the vlaues that have been recieved from the data generator in an index.
+  --indexpk - Hold the value of the peak value by index.
+  --index - Counts how many values have been recieved from the data generator.
+  --start_index - Records how many start signals have been recieved so that the corresponding byte can be sent to the command processor.
+  --numWordsValue - Stoeres the number of words that need to be processed by the data generator.
   
 
 begin
 ------------------------------------------------------
-  combi_nextState: process(clk, curState) --ctrlIn_detected, peakValueSmaller, start)
+  combi_nextState: process(clk, curState, start_enable, numWordsValue, index, ctrlIn_detected, peakValueSmaller)
   begin
     
     case curState is
        
+       --Reset state
        when S0 =>
+       --Checks to see if a start command has been recieved from the command processor.
         if start_enable='1' then
+          --Resets the counter, peak index and various enable signals.
           start_index <= 0;
           ctrlOut_reg <= '0';
           indexpk <= 0;
@@ -53,18 +72,21 @@ begin
           nextState <= S1;
         else null; 
         end if;
-            
-        
+       
+       --Checks to see if all of the data has been processed by the data generator.     
        when S1 => 
+         --If all of the data has been processed, the final data is passed to the command processor and the seqdone signal is asserted.
         if numWordsValue = index then
           seqDone <= '1';
           store_enable <= '1';
           nextState <= S0;
+         --If data generation has not finished, then the handshaking protocol starts.
         else
           ctrlOut_reg <= not ctrlOut_reg;
           nextState <= S2;
         end if; 
        
+       --Waits for the data generator to send back a signal before allowing the new data byte to be shifted as well as incrementing the counter.
        when S2 => 
         if ctrlIn_detected = '1' then   
           shift_enable <= '1';  
@@ -73,18 +95,21 @@ begin
         else
           nextState <= S2;
         end if;
-      
+       
+       --Adjusts the peak value if required.
        when S3 =>
         shift_enable <= '0';
         count_enable  <= '0'; 
-        if peakValueSmaller='1' then
+        --Checks to see if new value from the data generator is greater then the current peak value and if true then changes the peak value and it's index.
+        if peakValueSmaller = '1' then
           peakValue <= newValue;
           indexPk <= index;
         else null;
         end if;
         nextState <= S4;
-        
-      when S4 =>
+      
+       --Checks to see if a start signal has been put high and if true sends the next byte in sequence to the command processor. 
+       when S4 =>
         if start_enable ='1' then
           dataready <= '1';
           byte <= allData(start_index);
@@ -92,13 +117,12 @@ begin
         else null;
         end if;
         nextState <= S1;
-         
-        
-        
+                 
       end case;
-  end process; -- combi_nextState
+  end process; 
   
 ------------------------------------------------------  
+  --Checks to see if a start signal has been sent from the command processor during data generation.
   start_proc : process (clk, start)
   begin
     if rising_edge(clk) and start = '1' then
@@ -108,8 +132,8 @@ begin
   end process;
   
 ------------------------------------------------------
-
-  store: process (clk, store_enable)
+  --After data generation, the peak value and values three above and below are put into dataresults.
+  stored_result : process (clk, store_enable)
   begin
     if rising_edge(clk) and store_enable = '1' then
       for i in 6 downto 0 loop
@@ -119,18 +143,17 @@ begin
     end if;
   end process;   
         
-
 ------------------------------------------------------  
-  
+  --Converts the BCD value into an integer value that specifies the number of words to process.
   numWords : process (clk, numWords_bcd)
   begin
-     IndexEqualtoNumwords
+     --IndexEqualtoNumwords
      
-     numWordsValue <= numWords_bcd(0) * 100 + numWords_bcd(1) * 10 + numWords_bcd(2);
-  end process; -- numWords
+     --numWordsValue <= numWords_bcd(0) * 100 + numWords_bcd(1) * 10 + numWords_bcd(2);
+  end process; 
  
 ------------------------------------------------------  
- 
+  --Counts the index value for the number of words that have been processed.
   counter : process (clk, count_enable, count_reset)
   begin
       if count_reset = '1' then
@@ -141,7 +164,7 @@ begin
   end process; 
 
 ------------------------------------------------------
-  
+  --Shitfs data into the array that stores all hexadecimal values.
   shifting : process (clk, shift_enable, data)
   begin
       if rising_edge(clk) and shift_enable='1' then 
@@ -153,8 +176,8 @@ begin
       end if;
   end process;    
  
-
 ------------------------------------------------------
+  --Compares the new value with the current peak value and see which is greater.
   comparator: process(clk, peakValue, newValue)
   begin
     if peakValue = newValue then
@@ -167,20 +190,22 @@ begin
       equal <= '0';
       peakValueSmaller <= '1';  
     end if;
-   end process; --comparator 
+   end process; 
   
 ------------------------------------------------------
+  --Goes back to the first state when reset, else chnages state on a rising clock edge. 
   seq_state: process (clk, reset)
   begin
     if reset = '1' then
       curState <= S0;
-      
     elsif clk'event AND clk='1' then
       curState <= nextState;
       ctrlOut <= ctrlOut_reg;
     end if;
-  end process; -- seq
+  end process;
+  
 ------------------------------------------------------
+  --Hand shaking protocool for the communication with the data generator.
   delay_CtrlIn: process(clk)     
   begin
     if rising_edge(clk) then
@@ -193,3 +218,4 @@ begin
   newValue <= allData(0);
 ------------------------------------------------------
 end; --detectorArch
+
