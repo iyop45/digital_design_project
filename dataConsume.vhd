0d7a569163820951394342e35dba67c51852a128
Arastu Sharma <arastuindia@gmail.com>
2016-03-14 16:19:01 +0000

    Still has bcd and index errors

--- dataConsume.vhd
+++ dataConsume.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std_unsigned.all;
+--use ieee.numeric_std_unsigned.all;
 use work.common_pack.all;
 
 entity dataConsume is
@@ -27,8 +27,11 @@ architecture detectorArch of dataConsume is
   signal peakvalue, newValue: std_logic_vector(7 downto 0) := "00000000";
   signal ctrlOut_reg, equal, peakValueSmaller, shift_enable, store_enable, count_enable, count_reset, start_enable : std_logic:='0';  --bit := '0';
   signal ctrlIn_delayed, ctrlIn_detected: std_logic;
-  signal allData: CHAR_ARRAY_TYPE(0 to 999);  
-  signal indexpk, index, start_index, numWordsValue: integer:=0;
+  signal allData: CHAR_ARRAY_TYPE(0 to 998);  
+  signal indexpk, start_index, numWordsValue: integer:=0;
+  signal index: integer:= 0;
+  --signal debug: std_logic:= '0';
+  
   
   --curState - Current State.
   --nextState - next State.
@@ -53,7 +56,7 @@ architecture detectorArch of dataConsume is
 
 begin
 ------------------------------------------------------
-  combi_nextState: process(clk, curState, start_enable, numWordsValue, index, ctrlIn_detected, peakValueSmaller)
+  combi_nextState: process(clk, curState) --start_enable, numWordsValue, index, ctrlIn_detected, peakValueSmaller)
   begin
     
     case curState is
@@ -76,7 +79,7 @@ begin
        --Checks to see if all of the data has been processed by the data generator.     
        when S1 => 
          --If all of the data has been processed, the final data is passed to the command processor and the seqdone signal is asserted.
-        if numWordsValue = index then
+        if index = 300 then
           seqDone <= '1';
           store_enable <= '1';
           nextState <= S0;
@@ -102,7 +105,7 @@ begin
         count_enable  <= '0'; 
         --Checks to see if new value from the data generator is greater then the current peak value and if true then changes the peak value and it's index.
         if peakValueSmaller = '1' then
-          peakValue <= newValue;
+          peakValue <= allData(index);
           indexPk <= index;
         else null;
         end if;
@@ -131,6 +134,15 @@ begin
     end if;
   end process;
   
+---------------------------------------------------  
+  DataStore : process (clk, shift_enable, data)
+  begin
+      if rising_edge(clk) and shift_enable='1' then 
+        allData(index) <= data; 
+      else null;
+      end if;
+  end process;    
+  
 ------------------------------------------------------
   --After data generation, the peak value and values three above and below are put into dataresults.
   stored_result : process (clk, store_enable)
@@ -144,14 +156,14 @@ begin
   end process;   
         
 ------------------------------------------------------  
-  --Converts the BCD value into an integer value that specifies the number of words to process.
-  numWords : process (clk, numWords_bcd)
-  begin
-     --IndexEqualtoNumwords
-     
-     --numWordsValue <= numWords_bcd(0) * 100 + numWords_bcd(1) * 10 + numWords_bcd(2);
-  end process; 
- 
+  ----Converts the BCD value into an integer value that specifies the number of words to process.
+--  numWords : process (clk, numWords_bcd)
+--  begin
+--     --IndexEqualtoNumwords
+--     
+--     --numWordsValue <= numWords_bcd(0) * 100 + numWords_bcd(1) * 10 + numWords_bcd(2);
+--  end process; 
+-- 
 ------------------------------------------------------  
   --Counts the index value for the number of words that have been processed.
   counter : process (clk, count_enable, count_reset)
@@ -159,38 +171,41 @@ begin
       if count_reset = '1' then
         index <= 0;
       elsif rising_edge(clk) and count_enable='1' then 
-        index <= index + 1;
+       -- if debug = '1' then
+         index <= index + 1;
+        --else debug <= '1';
+        --end if; 
       end if;
   end process; 
 
 ------------------------------------------------------
   --Shitfs data into the array that stores all hexadecimal values.
-  shifting : process (clk, shift_enable, data)
-  begin
-      if rising_edge(clk) and shift_enable='1' then 
-        for i in 999 downto 1 loop 
-            allData(i) <= allData(i-1); 
-        end loop;
-        allData(0) <= data; 
-      else null;
-      end if;
-  end process;    
- 
+  --shifting : process (clk, shift_enable, data)
+--  begin
+--      if rising_edge(clk) and shift_enable='1' then 
+--        for i in 999 downto 1 loop 
+--            allData(i) <= allData(i-1); 
+--        end loop;
+--        allData(0) <= data; 
+--      else null;
+--      end if;
+--  end process;    
+-- 
 ------------------------------------------------------
   --Compares the new value with the current peak value and see which is greater.
-  comparator: process(clk, peakValue, newValue)
+  comparator: process(clk, peakValue, allData)
   begin
-    if peakValue = newValue then
-      equal <= '1';
-      peakValueSmaller <= '0';
-    elsif peakValue > newValue then 
-      equal <= '0';
-      peakValueSmaller <= '0';  
-    elsif peakValue < newValue then
-      equal <= '0';
-      peakValueSmaller <= '1';  
-    end if;
-   end process; 
+      if peakValue = allData(index) then
+        equal <= '1';
+        peakValueSmaller <= '0';
+      elsif peakValue > allData(index) then 
+        equal <= '0';
+        peakValueSmaller <= '0';  
+     elsif peakValue < allData(index) then
+       equal <= '0';
+       peakValueSmaller <= '1';  
+     end if;
+ end process; 
   
 ------------------------------------------------------
   --Goes back to the first state when reset, else chnages state on a rising clock edge. 
@@ -215,7 +230,7 @@ begin
   
   ctrlIn_detected <= ctrlIn xor ctrlIn_delayed;
 ------------------------------------------------------
-  newValue <= allData(0);
+ -- newValue <= allData(index);
 ------------------------------------------------------
 end; --detectorArch
 
