3b4329772ba46b4f515ce83627b85f5b66774ebf
woshua <jc14126@my.bristol.ac.uk>
2016-03-14 16:25:14 +0000

    Added files via upload

--- dataProc.vhd
+++ dataProc.vhd
@@ -1,102 +1,136 @@
+---author: joshua coop
 ---
---- author: joshua coop
---- processing the data recieved from the data processor to send to the Tx module
----
-
+--- processing the data processors data for P and L commands
 library ieee;
 use ieee.std_logic_1164.all;
---use ieee.std_logic_arith.all;
-use ieee.numeric_std_unsigned.all;
-
-use ieee.numeric_std.all; -- additional debug
+use ieee.std_logic_arith.all;
 
 use work.common_pack.all;
 
-entity dataProc is
+entity Pcmd is
 	port (
 		clk:		in std_logic;
 		reset:		in std_logic;
-		
+		rxnow:		in std_logic; -- valid port
+		rxData:			in std_logic_vector (7 downto 0);
 		txData:			out std_logic_vector (7 downto 0);
+		rxdone:		out std_logic;
+		ovErr:		in std_logic;
+		framErr:	in std_logic;
 		txnow:		out std_logic;
 		txdone:		in std_logic;
-		
 		start: out std_logic;
+		--numWords: out std_logic_vector(9 downto 0);
+		numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
 		dataReady: in std_logic;
 		byte: in std_logic_vector(7 downto 0);
-		
-		cmdNow: in std_logic;     
-		cmdDone: out std_logic; 
-		cmdRecieve: out std_logic;
-		
-		prntNow: out std_logic;
-		prntSpace: out std_logic
+		maxIndex: in BCD_ARRAY_TYPE(12 downto 0);
+--		dataResults: in std_logic_vector(55 downto 0);
+		dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
+		seqDone: in std_logic;
+	  --inter module signals
+	  PReceive : in std_logic;
+	  PDone : out std_logic
+	   
 	);
-end dataProc;
+end Pcmd;
 
-architecture processData of dataProc is
-  type state_type is (S0, S1, S2, S3, S4);
+  architecture Pcommand of Pcmd is
+  type state_type is (S0, S1, S2, S3, S4, S5, S6, S7);
   signal curState, nextState: state_type;
-begin
-
-	combi_nextState: process(clk, curState)
-	begin
-		case curState is
-			when S0 =>
-				cmddone <= '0';
-				txnow <= '0';       
-				if txdone = '1' AND cmdNow = '1' then --- roys part telling me that he's recieved an annn and i should start processing
-					nextstate <= S1;
-				else
-					nextstate <= S0;
-				end if;
-				
-			when S1 =>
-				cmdRecieve <= '1'; --- variable to tell roy i have started processing
-				start <= '1';
-				nextstate <= S2;
-				
-      when S2 =>
-        start <= '0';
-        if cmdNow = '0' then
-			     cmdRecieve <= '0';
-			     nextState <= S3;
-        else 
-			     cmdRecieve <= '1';
-			     nextState <= S2;
-        end if;
-				
-			when S3 =>
-				start <= '0';
-				if dataready = '1' then
-					nextstate <= S4;
-				else
-					nextstate <= S0;        
-				end if;
-				
-			when S4 =>
-				cmdRecieve <= '0';
-				cmdDone <= '1';
-				txnow <= '1';
-				txData <= byte;
-				nextstate <= S0;
-				
-			when others =>
-			  nextstate <= S0;
-			
-		end case;
-	end process; -- datasend
-	-----------------------------------------------------
-	seq_state: process (clk, reset)
-	  begin
-		if reset = '1' AND clk'event AND clk='1' then
-		  curState <= S0;
-		elsif clk'event AND clk='1' then
-			curState <= nextState;
-		end if;
-	end process; -- stateChange
-	-----------------------------------------------------  
+	signal counter_enable : std_logic := '0';
+	signal counter_reset : std_logic := '0';
+	signal count : integer := 0;
 	
-end; -- processData
-
+begin
+  dataSend: process(txdone, dataready, byte)
+  begin
+    case curState is
+--- handshake to the module to ensure tha the rxdata has recieved an L or 1
+     when  S0 => 
+     counter_reset <= '1'; 
+     TxNow <= '0';     
+       if PReceive = '1' then 
+         nextstate <= S1;
+       else
+         nextstate <= S0;
+       end if;
+       
+--- handshake back to the cmdparce to say that we know that an L or 1 has been recieved        
+      when S1 =>
+     counter_reset <= '0';
+       PDone <= '1';
+       if PReceive = '0' then 
+         nextstate <= S2;
+       else
+         nextstate <= S1;
+       end if;
+       
+--- sends the data results values in order, from count = 0 to 7       
+      when S2 =>
+        TxData <= dataResults(3);
+        TxNow <= '1';
+        PDone <= '0';
+        if TxDone = '1' then
+          nextstate <= S3;
+        else
+          nextstate <= S2;
+        end if;
+        
+---       
+      when S3 =>
+        TxNow <= '0';
+        nextstate <= S4;
+        
+--- sends a space to the output and then tests to see if all the dataresults have been sent       
+      when S4 =>
+          TxData <= x"50"; -- space at the output
+          TxNow <= '1';
+          if TxDone = '1' then
+            nextstate <= S5;
+          else
+            nextstate <= S4;
+          end if;
+        
+---       
+      when S5 =>
+        TxNow <= '0';
+        nextstate <= S6;
+        
+      when S6 =>
+          TxData <= "0011"& maxIndex(count+3 to count);
+          TxNow <= '1';
+          if TxDone = '1' then
+            if count = 2 then
+              nextstate <= S0;
+            else
+              nextstate <= S7;
+            end if;       
+          else
+            nextstate <= S6;
+          end if;        
+        
+      when S7 =>
+        counter_enable <= '1';
+        TxNow <= '0';
+        nextstate <= S6;
+        
+--      when others =>
+--        nextstate <= S0;
+        
+    end case;
+ end process;
+   ----------------------------------------------------
+   
+   ---------------------------------------------------- 
+    stateChange: process (clk, reset)
+  begin
+    if reset = '1' AND clk'event AND clk='1' then
+      curState <= S0;
+    elsif clk'event AND clk='1' then
+    curState <= nextState;
+    end if;
+  end process; --clock
+  -----------------------------------------------------
+end;
 
