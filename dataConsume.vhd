c67c1ee81b8a3ba2d6a8787ecc7224fd15837649
Arastu Sharma <arastuindia@gmail.com>
2016-03-16 18:25:50 +0000

    Please God Work, dammit!

--- dataConsume.vhd
+++ dataConsume.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
 use work.common_pack.all;
 
 entity dataConsume is
@@ -25,15 +24,15 @@ architecture detectorArch of dataConsume is
   
   type state_type is (S0, S1, S2, S3, S4);
   signal curState, nextState: state_type;
-  signal peakvalue, newValue: std_logic_vector(7 downto 0) := (others => '0');
-  signal ctrlOut_reg, equal, peakValueSmaller, shift_enable, store_enable, count_enable, count_reset, start_enable : std_logic := '0';  --bit := '0';
+  signal peakvalue, newValue: std_logic_vector(7 downto 0) := "00000000";
+  signal ctrlOut_reg, equal, peakValueSmaller, shift_enable, store_enable, count_enable, count_reset, start_enable : std_logic:='0';  --bit := '0';
   signal ctrlIn_delayed, ctrlIn_detected: std_logic;
   signal allData: CHAR_ARRAY_TYPE(0 to 998);  
-  signal indexpk, start_index, index, numWordsValue: integer := 0;
+  signal index, indexpk, start_index, numWordsValue: integer:=0;
+  signal counter1, counter2, counter3: std_logic_vector(3 downto 0) := "0000";
   signal debug: std_logic:= '0';
-  signal numWords: std_logic_vector(15 downto 0);
-  signal stdIndexPk: std_logic_vector(9 downto 0);
-  signal maxIndexSignal: std_logic_vector(11 downto 0) := (others => '0');
+  signal counterOut, numWords, indexPk_bcd: std_logic_vector(11 downto 0);
+  
   
   --curState - Current State.
   --nextState - next State.
@@ -83,15 +82,20 @@ begin
        when S1 => 
           debug <= '1';
          --If all of the data has been processed, the final data is passed to the command processor and the seqdone signal is asserted.
-        if index = 500 then
+        if counterOut = numWords then
           seqDone <= '1';
           store_enable <= '1';
+          maxIndex(0) <= indexPk_bcd(11 downto 8);
+          maxIndex(1) <= indexPk_bcd(7 downto 4);
+          maxIndex(2) <= indexPk_bcd(3 downto 0);
           nextState <= S0;
+          
          --If data generation has not finished, then the handshaking protocol starts.
         else
           ctrlOut_reg <= not ctrlOut_reg;
           nextState <= S2;
         end if; 
+       
        
        --Waits for the data generator to send back a signal before allowing the new data byte to be shifted as well as incrementing the counter.
        when S2 => 
@@ -103,6 +107,7 @@ begin
           nextState <= S2;
         end if;
        
+       
        --Adjusts the peak value if required.
        when S3 =>
         shift_enable <= '0';
@@ -111,6 +116,7 @@ begin
         if peakValueSmaller = '1' then
           peakValue <= allData(index-1);
           indexPk <= index-1;
+          indexPk_bcd <= counterOut;
         elsif debug = '1' then
           peakValue <= allData(0);
           indexPk <= 0;
@@ -132,7 +138,9 @@ begin
                  
       end case;
   end process; 
-  
+
+  counterOut <= counter3 & counter2 & counter1;
+  numWords <= numWords_bcd(2)&numWords_bcd(1)&numWords_bcd(0);
 ------------------------------------------------------  
   --Checks to see if a start signal has been sent from the command processor during data generation.
   start_proc : process (clk, start)
@@ -164,66 +172,49 @@ begin
   end process;   
         
 ------------------------------------------------------  
-
-  -- Double Dabble method is used here to convert integer into a BCD
-  int_to_bcd : process (clk, indexpk)
-  begin
-    stdIndexPk <= std_logic_vector(to_unsigned(indexpk, 10));
-    
-    for i in 0 to 9 loop -- repeating 9 times as 999 is a 10 bit value
-      --we shift the bits left from the stdIndex to the numWords that contains the bcd
-      maxIndexSignal(11 downto 1) <= maxIndexSignal(10 downto 0); 
-      maxIndexSignal(0) <= stdIndexPk(9);
-      stdIndexPk(9 downto 1) <= stdIndexPk(8 downto 0);
-      -- 0 is added at the end of the stdIndexPk as its shifts to left
-      stdIndexPk(9) <= '0';
-      -- As per double dabble, 3 is added to BCD if a BCD digit is greater than 4
-      if(maxIndexSignal(3 downto 0) > "0100") then 
-        maxIndexSignal(3 downto 0) <= maxIndexSignal(3 downto 0) + "0011";
-      end if;
-
-      if(maxIndexSignal(7 downto 4) > "0100") then 
-        maxIndexSignal(7 downto 4) <= maxIndexSignal(7 downto 4) + "0011";
-      end if;
-
-      if(maxIndexSignal(11 downto 8) > "0100") then
-        maxIndexSignal(11 downto 8) <= maxIndexSignal(11 downto 8) + "0011";
-      end if;
-
-    end loop;
-    
-    maxIndex(2) <= maxIndexSignal(3 downto 0);
-    maxIndex(1) <= maxIndexSignal(7 downto 4);
-    maxIndex(0) <= maxIndexSignal(11 downto 8);
-    
-    
-  end process; 
- 
---------------------------------------------------------
---  -- Converts the BCD value into an integer value that specifies the number of words to process.
---  bcd_to_int : process (clk, index)
+--  --Counts the index value for the number of words that have been processed.
+--  counter : process (clk, count_enable, count_reset)
 --  begin
---    for i in 0 to 11 then
---      if (i>=0 and i<4)
---        numWords
---  end process;
------------------------------------------------------- 
-  --Counts the index value for the number of words that have been processed.
-  counter : process (clk, count_enable, count_reset)
+--      if count_reset = '1' then
+--        index <= 0;
+--      elsif rising_edge(clk) and count_enable='1' then 
+--       -- if debug = '1' then
+--         index <= index + 1;
+--         start_index <= start_index+1; 
+--        --else debug <= '1';
+--        
+--      end if;
+--  end process; 
+  
+------------------------------------------------------  
+--Thanks Josh
+  counter: process (clk, count_enable, count_reset)
   begin
-      if count_reset = '1' then
-        index <= 0;
-      elsif rising_edge(clk) and count_enable='1' then 
-       -- if debug = '1' then
-         index <= index + 1;
-         start_index <= start_index+1; 
-        --else debug <= '1';
+    if count_reset = '1' then
         
-      end if;
-  end process; 
+        counter3 <= "0000";
+        counter2 <= "0000";
+        counter1 <= "0000";
+        
+    elsif count_enable = '1' then
+        index <= index + 1;
+        start_index <= start_index+1;
+        counter1 <= std_logic_vector(unsigned(counter1) + "0001");
+        if counter1 = "1001" then
+           counter2 <= std_logic_vector(unsigned(counter2) + "0001");
+           counter1 <= "0000";
+           if counter2 = "1001" then
+              counter3 <= std_logic_vector(unsigned(counter3) + "0001");
+              counter2 <= "0000";
+              if counter3 = "1001" then
+                 counter3 <= "0000";
+              end if;
+            end if;
+        end if;
+     end if;
+  end process;
 
 ------------------------------------------------------
- 
   --Compares the new value with the current peak value and see which is greater.
   comparator: process(clk, peakValue, allData, index)
   begin
