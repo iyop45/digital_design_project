a444449f3c3e283703421bae4f96474130589d05
woshua <jc14126@my.bristol.ac.uk>
2016-03-16 17:09:44 +0000

    changed the code from push button to locking \o/

--- dataProc.vhd
+++ dataProc.vhd
@@ -24,15 +24,16 @@ entity dataProc is
 		start: out std_logic;
 		dataReady: in std_logic;
 		byte: in std_logic_vector(7 downto 0);
+		seqDone: in std_logic;
 		
 		cmdNow: in std_logic;     --- goes high when a start command is typed into the terminal of the format aNNN or ANNN
 		cmdRecieve: out std_logic; --- send back to tell cmdparse that data is not being processed by this module
-		dataProc_Hold: out std_logic
+		TxHold: out std_logic
 	);
 end dataProc;
 
 architecture processData of dataProc is
-  type state_type is (S0, S1, S2, S3, S4, S5, S6);
+  type state_type is (S0, S1, S2, S3, S4, S5, S6, S7);
   signal curState, nextState: state_type;
 begin
 
@@ -40,32 +41,30 @@ begin
 	begin
 		case curState is
 			when S0 =>
-				stxNow <= '0'; 
-				dataProc_Hold <= '0';
+				TxHold <= '0';
         --- tells us when a new byte can be sent through the Tx module and that a start has been sent through the Rx module
-				if  cmdNow = '1' AND stxDone = '1' then 
+				if  cmdNow = '1' then 
 				  nextstate <= S1;
 				else
 					nextstate <= S0;
 				end if;
 				
-			when S1 =>
-			  dataProc_Hold <= '1';
-				cmdRecieve <= '1'; 
+      when S1 =>
+    				cmdRecieve <= '1';
+        if cmdNow = '0' then --- a duel handshake between cmd parse and this module
+			     nextState <= S2;
+        else 
+			     nextState <= S1;
+        end if;
+				
+			when S2 =>
+			  TxHold <= '1';
+				cmdRecieve <= '0'; 
 				start <= '1';
 				nextstate <= S2;
 				
-      when S2 =>
-        start <= '0';
-        if cmdNow = '0' then --- a duel handshake between cmd parse and this module
-			     cmdRecieve <= '0';
-			     nextState <= S3;
-        else 
-			     cmdRecieve <= '1';
-			     nextState <= S2;
-        end if;
-				
 			when S3 => --- waits until there is a new byte of data from the data processor
+			  start <= '0';
 				if dataready = '1' then
 					nextstate <= S4;
 				else
@@ -73,7 +72,6 @@ begin
 				end if;
 				
 			when S4 => 
-				cmdRecieve <= '0';
 				stxData <= byte; --- gives the byte given by the data procossor to the Tx module
 				stxNow <= '1'; ---issues a send command
 				nextstate <= S5;
@@ -82,7 +80,6 @@ begin
 			when S5 => --sets stxNow to 0 and waits to issue the next send			  
 			  stxNow <= '0'; 
 			  if stxDone = '1' then --- waits until Tx modules ready to send again
-			    
 				  nextstate <= S6;
 				else
 				  nextstate <= S5;
@@ -91,10 +88,15 @@ begin
 			when S6 =>-- adds a space after every character and issue a send command
 			  stxData <= x"50";
 			  stxNow <= '1';
-				nextstate <= S0;
+			  nextstate <= S7;
 
-			when others =>
-			  nextstate <= S0;
+			when S7 =>
+			  stxNow <= '0'; 
+			  if seqDone = '1' then
+				   nextstate <= S0;
+				else 
+				   nextstate <= S2;
+				end if;
 			
 		end case;
 	end process; -- datasend
